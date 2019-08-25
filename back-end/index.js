5846a98e6fd2a9d390aa7ba39414f8b4838e01ee
Clark <rclark010@gmail.com>
2019-08-25 15:56:05 -0600

    Moving files

--- back-end/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/server');
-
-const port = process.env.PORT || 3300;
-
-server.listen(port, () => 
-	console.log(`\n*** Server Running on Port ${port}! ***\n`)
-);
